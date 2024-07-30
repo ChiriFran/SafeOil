5a08685337b5270d136414af7899d34c7f565077
Chiri Francisco <franchiri00@gmail.com>
2024-07-30 13:56:32 -0300

    vs

---
+++ assets/scripts/serviciosCardAnimn.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelectorAll(".toggleButton").forEach((button) => {
+    button.addEventListener("click", () => {
+      const item = button.closest(".serviciosItem");
+      const text = item.querySelector(".serviciosItemsText");
+
+      item.classList.toggle("open");
+      button.classList.toggle("open");
+
+      if (item.classList.contains("open")) {
+        text.style.maxHeight = "300px"; // Altura de despliegue fija
+        text.style.opacity = 1; // Asegura que el texto sea visible
+      } else {
+        text.style.maxHeight = 0; // Altura cuando está colapsado
+        text.style.opacity = 0; // Oculta el texto
+      }
+    });
+  });
+});
